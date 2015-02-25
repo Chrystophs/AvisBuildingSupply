3cbbf051a04e3a4ca224fc16a3496a4811f39e2e
Kitzo <absuser@FrontCounter.ABS.local>
2015-02-25 15:29:30 -0500

    minor Change

--- wp-content/themes/gpm-bas3-them3-sass/page.php
+++ wp-content/themes/gpm-bas3-them3-sass/page.php
@@ -37,6 +37,13 @@
                                 <?php } ?>
                                 
                                 <?php the_content(); ?>
+                                <ul>
+<?php $the_query = new WP_Query( 'showposts=5' ); ?>
+<?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+<li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
+<li><?php the_content(__('(more…)')); ?></li>
+<?php endwhile;?>
+</ul>
                             </section>
                         </article>
                     </div>			
